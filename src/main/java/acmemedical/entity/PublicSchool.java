6dc4be18e2e31afda859e594e9f8ef6bcb2bf762
Azadeh Sadeghtehrani <sade0041@algonquinlive.com>
2024-11-14 10:19:11 -0500

    Finished my part in task1

--- src/main/java/acmemedical/entity/PublicSchool.java
+++ src/main/java/acmemedical/entity/PublicSchool.java
@@ -8,9 +8,16 @@
 package acmemedical.entity;
 
 import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 
-//TODO PUSC01 - Add missing annotations, please see Week 9 slides page 15.  Value 1 is public and value 0 is private.
-//TODO PUSC02 - Is a JSON annotation needed here?
+//PUSC01 - Add missing annotations; value 1 is public and value 0 is private.
+@Entity
+@DiscriminatorValue("1")  // Specifies the value to be used for the "discriminator" column in the database for this entity type.
+
+//PUSC02 - Add JSON annotation if needed.
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class PublicSchool extends MedicalSchool implements Serializable {
 	private static final long serialVersionUID = 1L;
 
