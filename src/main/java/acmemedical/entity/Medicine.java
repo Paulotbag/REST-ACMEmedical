9cdd2dcf4e50558ea44d62527c775f1e556834a0
ShadowStorm6996 <shadowabraham50@gmail.com>
2024-11-13 20:56:09 -0500

    Done

--- src/main/java/acmemedical/entity/Medicine.java
+++ src/main/java/acmemedical/entity/Medicine.java
@@ -1,7 +1,7 @@
 /********************************************************************************************************
  * File:  Medicine.java Course Materials CST 8277
  *
- * @author Teddy Yap
+ * Author: Teddy Yap
  * 
  */
 package acmemedical.entity;
@@ -28,30 +28,14 @@ import jakarta.persistence.Transient;
 /**
  * The persistent class for the medicine database table.
  */
-//Hint - @Entity marks this class as an entity which needs to be mapped by JPA.
-//Hint - @Entity does not need a name if the name of the class is sufficient.
-//Hint - @Entity name does not matter as long as it is consistent across the code.
 @Entity
-//Hint - @Table defines a specific table on DB which is mapped to this entity.
 @Table(name = "medicine") 
-//Hint - @NamedQuery attached to this class which uses JPQL/HQL.  SQL cannot be used with NamedQuery.
-//Hint - @NamedQuery uses the name which is defined in @Entity for JPQL, if no name is defined use class name.
-//Hint - @NamedNativeQuery can optionally be used if there is a need for SQL query.
 @NamedQuery(name = "Medicine.findAll", query = "SELECT m FROM Medicine m")
-//Hint - @AttributeOverride can override column details.  This entity uses medicine_id as its primary key name, it needs to override the name in the mapped super class.
 @AttributeOverride(name = "id", column = @Column(name = "medicine_id"))
-//Hint - PojoBase is inherited by any entity with integer as their primary key.
-//Hint - PojoBaseCompositeKey is inherited by any entity with a composite key as their primary key.
 public class Medicine extends PojoBase implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	// Hint - @Basic(optional = false) is used when the object cannot be null.
-	// Hint - @Basic or none can be used if the object can be null.
-	// Hint - @Basic is for checking the state of object at the scope of our code.
 	@Basic(optional = false)
-	// Hint - @Column is used to define the details of the column which this object will map to.
-	// Hint - @Column is for mapping and creation (if needed) of an object to DB.
-	// Hint - @Column can also be used to define a specific name for the column if it is different than our object name.
 	@Column(name = "drug_name", nullable = false, length = 50)
 	private String drugName;
 
@@ -63,20 +47,13 @@ public class Medicine extends PojoBase implements Serializable {
 	@Column(name = "dosage_information", nullable = false, length = 100)
 	private String dosageInformation;
 
-	// Hint - @Transient is used to annotate a property or field of an entity class, mapped superclass, or embeddable class which is not persistent.
 	@Transient
 	private String chemicalName;
 	
 	@Transient
 	private String genericName;
 
-	// Hint - @OneToMany is used to define 1:M relationship between this entity and another.
-	// Hint - @OneToMany option cascade can be added to define if changes to this entity should cascade to objects.
-	// Hint - @OneToMany option cascade will be ignored if not added, meaning no cascade effect.
-	// Hint - @OneToMany option fetch should be lazy to prevent eagerly initializing all the data.
-	@OneToMany(cascade=CascadeType.MERGE, fetch = FetchType.LAZY, mappedBy = "medicine")
-	// Hint - java.util.Set is used as a collection, however List could have been used as well.
-	// Hint - java.util.Set will be unique and also possibly can provide better get performance with HashCode.
+	@OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY, mappedBy = "medicine")
 	private Set<Prescription> prescriptions = new HashSet<>();
 
 	public Medicine() {
@@ -139,12 +116,44 @@ public class Medicine extends PojoBase implements Serializable {
 		this.prescriptions = prescriptions;
 	}
 
+	// Helper method to add a Prescription to the Medicine
+	public void addPrescription(Prescription prescription) {
+		this.prescriptions.add(prescription);
+		prescription.setMedicine(this);
+	}
+
+	// Helper method to remove a Prescription from the Medicine
+	public void removePrescription(Prescription prescription) {
+		this.prescriptions.remove(prescription);
+		prescription.setMedicine(null);
+	}
+
 	public void setMedicine(String drugName, String manufacturerName, String dosageInformation) {
 		setDrugName(drugName);
 		setManufacturerName(manufacturerName);
 		setDosageInformation(dosageInformation);
 	}
 
-	//Inherited hashCode/equals is sufficient for this entity class
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId(), drugName, manufacturerName);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Medicine))
+			return false;
+		Medicine other = (Medicine) obj;
+		return Objects.equals(getId(), other.getId()) &&
+			   Objects.equals(drugName, other.drugName) &&
+			   Objects.equals(manufacturerName, other.manufacturerName);
+	}
+
+	@Override
+	public String toString() {
+		return "Medicine [id=" + getId() + ", drugName=" + drugName + ", manufacturerName=" + manufacturerName +
+			   ", dosageInformation=" + dosageInformation + "]";
+	}
 }
